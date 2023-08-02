115cba5b936cbd3ff9f451da4e954309cc1f9f0f
Guido Almada <guido.almada.91@gmail.com>
2023-08-02 15:10:23 -0300

    Corrección

--- Quini_6/quini_6.py
+++ Quini_6/quini_6.py
@@ -1,7 +1,10 @@
 import random
+
 numeros_aleatorios = []
-for i in range(6):
-    quini = random.randrange(0, 46)
-    numeros_aleatorios.append(quini)
-    numeros_aleatorios.sort()
+while len(numeros_aleatorios) < 6:
+    quini = random.randrange(1, 46)
+    if quini not in numeros_aleatorios:
+        numeros_aleatorios.append(quini)
+
+numeros_aleatorios.sort()
 print(numeros_aleatorios)
