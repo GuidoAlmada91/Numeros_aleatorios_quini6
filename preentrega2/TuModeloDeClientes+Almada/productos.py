54bbd6aca36608cf6e771b8e51606e3911d3dc3e
GuidoAlmada91 <140521142+GuidoAlmada91@users.noreply.github.com>
2023-08-07 13:01:00 -0300

    Delete preentrega2 directory

--- preentrega2/TuModeloDeClientes+Almada/productos.py
+++
@@ -1,8 +0,0 @@
-class Productos:
-    def __init__ (self, producto, precio):
-        self.producto = producto
-        self.precio = precio
-
-    def __str__(self):
-        return f"Producto: {self.producto} $:{self.precio}"
-        
