45b8c5c3c20438937111b5d695967c2399004e45
GuidoAlmada91 <140521142+GuidoAlmada91@users.noreply.github.com>
2023-08-07 12:55:45 -0300

    Delete microdesafios directory

--- microdesafios/importando_microdesafio.py
+++
@@ -1,6 +0,0 @@
-from microdesafio_modulo import Alumno
-alumno = Alumno("Guido", 7.5)
-print(alumno)
-
-
-    
--- microdesafios/microdesafio_modulo.py
+++
@@ -1,7 +0,0 @@
-class Alumno:
-    def __init__(self, nombre, nota):
-        self.nombre = nombre
-        self.nota = nota
-    def __str__(self):
-        return f"Nombre: {self.nombre, self.nota}"
-    
